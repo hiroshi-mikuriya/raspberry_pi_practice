1e81b4e002f6145ab316447b7b7b43d7de0f5cce
mikuriya <mickly9@hotmail.com>
2018-03-18 17:08:47 +0900

    mod

--- spi2.rb
+++ spi2.rb
@@ -7,14 +7,21 @@ end
 
 CS0 = 0
 
-tx = %w[1 2 3 4].map(&:hex).pack('C*')
+def write_spi(tx)
+  rx = ([0] * tx.size).pack('C*')
+  BCM.bcm2835_spi_begin
+  BCM.bcm2835_spi_setBitOrder(1) # MSB First
+  BCM.bcm2835_spi_setDataMode(0) # CPOL = 0, CPHA = 0
+  BCM.bcm2835_spi_setClockDivider(64) # 64 = 256ns = 3.90625MHz
+  BCM.bcm2835_spi_chipSelect(CS0) # Chip Select 0
+  BCM.bcm2835_spi_setChipSelectPolarity(CS0, 0) # LOW
+  BCM.bcm2835_spi_writenb(tx, tx.size)
+  BCM.bcm2835_spi_end
+end
 
-BCM.bcm2835_spi_begin
-BCM.bcm2835_spi_setBitOrder(1) # MSB First
-BCM.bcm2835_spi_setDataMode(0) # CPOL = 0, CPHA = 0
-BCM.bcm2835_spi_setClockDivider(64) # 64 = 256ns = 3.90625MHz
-BCM.bcm2835_spi_chipSelect(CS0) # Chip Select 0
-BCM.bcm2835_spi_setChipSelectPolarity(CS0, 0) # LOW
-BCM.bcm2835_spi_writenb(tx, tx.size)
-BCM.bcm2835_spi_end
+loop do
+  tx = %w[1 2 3 4].map(&:hex).pack('C*')
+  write_spi(tx)
+end
+
 BCM.bcm2835_close
