77b019a358c1b8f101c1a31e05cc854597111f50
mikuriya <mickly9@hotmail.com>
2018-03-18 15:05:01 +0900

    rm transmit function

--- spi_cpp/main.cpp
+++ spi_cpp/main.cpp
@@ -3,13 +3,16 @@
 #include <vector>
 #include <bcm2835.h>
 
-namespace
+int main(int argv, char ** argc)
 {
-    void transmit(std::string const & tx)
-    {
-        if (!bcm2835_init()){
-            return;
-        }        
+    if (!bcm2835_init()){
+        std::cerr << "failed bcm2835_init()" << std::endl;
+        return 1;
+    }        
+    for(;;) {
+        std::string s;
+        std::cin >> s;
+        transmit(s);
         bcm2835_spi_begin();
         bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
         bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default. Clock polarity = 0(Positive), Clock phase = 0(Positive)
@@ -17,18 +20,9 @@ namespace
         //bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); // Not default. Divide 64 = 256ns = 3.90625MHz
         bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
         bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default        
-        bcm2835_spi_writenb(const_cast<char*>(tx.data()), tx.size());        
+        bcm2835_spi_writenb(const_cast<char*>(s.data()), s.size());        
         bcm2835_spi_end();
         bcm2835_close();
-    }
-}
-
-int main(int argv, char ** argc)
-{
-    for(;;) {
-        std::string s;
-        std::cin >> s;
-        transmit(s);
     }
     return 0;
 }
