9fdd621383f82dc4bdc35b11b0524db4980a22da
mikuriya <mickly9@hotmail.com>
2018-03-18 20:09:40 +0900

    add AQUA, YELLOW and PINK

--- spi.rb
+++ spi.rb
@@ -7,9 +7,12 @@ WHITE = [WRITE, ADDR, [0xFF] * 96].flatten.pack('c*').freeze
 BLUE = [WRITE, ADDR, ([0xFF] * 4 + [0] * 8) * 8].flatten.pack('c*').freeze
 GREEN = [WRITE, ADDR, ([0] * 4 + [0xFF] * 4 + [0] * 4) * 8].flatten.pack('c*').freeze
 RED = [WRITE, ADDR, ([0] * 8 + [0xFF] * 4) * 8].flatten.pack('c*').freeze
+AQUA = [WRITE, ADDR, ([0xFF] * 8 + [0] * 4) * 8].flatten.pack('c*').freeze
+YELLOW = [WRITE, ADDR, ([0] * 4 + [0xFF] * 8) * 8].flatten.pack('c*').freeze
+PINK = [WRITE, ADDR, ([0xFF] * 4 + [0] * 4 + [0xFF] * 4) * 8].flatten.pack('c*').freeze
 
 loop do
-  [CLEAR, WHITE, BLUE, GREEN, RED].each do |color|
+  [CLEAR, WHITE, BLUE, GREEN, RED, AQUA, YELLOW, PINK].each do |color|
     SPI.write(color, SPI::CS0)
     sleep(1)
   end
