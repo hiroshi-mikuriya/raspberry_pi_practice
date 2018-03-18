238cba876196f4968b86579b51a7d74bc3212ad3
hiroshi-mikuriya <mickly9@hotmail.com>
2018-03-18 06:15:48 +0000

    use myspi (spi.rb

--- spi.rb
+++ spi.rb
@@ -1,7 +1,9 @@
 require 'open3'
 
-Open3.popen('./myspi') do |io|
-  d = %w[1 2 3 4].map(&:hex).pack('C*')
-  io.puts d
-  sleep(1)
+Open3.popen2('./myspi') do |i, _o|
+  loop do
+    d = %w[1 2 3 4].map(&:hex).pack('C*')
+    i.write d
+    sleep(1)
+  end
 end
