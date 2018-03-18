b26823259660681b6b7325ccb67458b3cd1aaf05
mikuriya <mickly9@hotmail.com>
2018-03-18 12:10:35 +0900

    to simple

--- spi.rb
+++ spi.rb
@@ -1,15 +1,4 @@
-require 'pi_piper'
-
-RESOLUTION_AT_12BIT = 0b111111111111.to_f.freeze
-
-PiPiper::Spi.begin do |spi|
-  loop do
-    _, center, last = spi.write [0b00000110, 0b00000000, 0b00000000]
-    center &= 0b00001111
-    center = center << 8
-    value =  center + last
-    percentage = value / RESOLUTION_AT_12BIT * 100
-    puts "#{percentage} %"
-    sleep 0.1
-  end
-end
+require 'spi'
+s = SPI.new(device: '/dev/spidev32766.0')
+s.speed = 500_000
+s.xfer(txdata: [0x10, 0x00])
