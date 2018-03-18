6ecb1d65f195133b27de7c1c37bd69e4253e6f8c
mikuriya <mickly9@hotmail.com>
2018-03-18 17:41:04 +0900

    rm redundant files

--- spi.rb
+++ spi.rb
@@ -1,9 +1,30 @@
-require 'open3'
+require'./bcm2835'
 
-Open3.popen2('./myspi') do |i, _o|
-  loop do
-    d = %w[1 2 3 4].map(&:hex).pack('C*')
-    i.write d
-    sleep(1)
-  end
+if BCM.bcm2835_init.zero?
+  warn 'failedtoinitbcm2835'
+  exit1
 end
+
+CS0 = 0
+
+def write_spi(tx)
+  rx = ([0] * tx.size).pack('C*')
+  BCM.bcm2835_spi_begin
+  BCM.bcm2835_spi_setBitOrder(1) # MSB First
+  BCM.bcm2835_spi_setDataMode(0) # CPOL = 0, CPHA = 0
+  BCM.bcm2835_spi_setClockDivider(64) # 64 = 256ns = 3.90625MHz
+  BCM.bcm2835_spi_chipSelect(CS0) # Chip Select 0
+  BCM.bcm2835_spi_setChipSelectPolarity(CS0, 0) # LOW
+  # rx = BCM.bcm2835_spi_transfer(tx)
+  BCM.bcm2835_spi_transfernb(tx, rx, tx.size)
+  puts tx == rx ? 'success' : "error #{tx} != #{rx}"
+  BCM.bcm2835_spi_end
+end
+
+loop do
+  tx = %w[2 0 0 55 AA].map(&:hex).pack('c*')
+  write_spi(tx)
+  sleep(1)
+end
+
+BCM.bcm2835_close
