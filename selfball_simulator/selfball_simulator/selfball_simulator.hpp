372054f3c6f6f7ded63dd4c0e86f51e90a36e0d7
hiroshi <hiroshi@iMacHiroshi2.local>
2018-04-21 18:52:43 +0900

    add simulator

---
+++ selfball_simulator/selfball_simulator/selfball_simulator.hpp
@@ -0,0 +1,22 @@
+//
+//  selfball_simulator.hpp
+//  selfball_simulator
+//
+//  Created by hiroshi on 2018/04/21.
+//  Copyright © 2018年 fxat. All rights reserved.
+//
+
+#ifndef selfball_simulator_
+#define selfball_simulator_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class selfball_simulator
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ selfball_simulator/selfball_simulator/selfball_simulatorPriv.hpp
@@ -0,0 +1,18 @@
+//
+//  selfball_simulatorPriv.hpp
+//  selfball_simulator
+//
+//  Created by hiroshi on 2018/04/21.
+//  Copyright © 2018年 fxat. All rights reserved.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class selfball_simulatorPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
